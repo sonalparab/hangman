733d0c86646a7f6b5041d51c1e8bae7280c0ef38
Helen Ye <00yhelen@gmail.com>
2018-01-19 10:32:49 -0500

    Remove diagnostic print

--- client.c
+++ client.c
@@ -10,29 +10,27 @@ int main() {
 
     from_server = client_handshake( &to_server );
 
-    printf("[client] reading...");
     while (read(from_server, buffer, BUFFER_SIZE)) {
         if (strcmp(buffer, PROMPT) == 0) {
             printf(PROMPT);
-	    fgets(input, sizeof(input), stdin);
-	    *strchr(input, '\n') = 0;
+            fgets(input, sizeof(input), stdin);
+            *strchr(input, '\n') = 0;
             write(to_server, input, sizeof(input));
             printf("[client] wrote input: [%s]\n", input);
-	    /*
-            fgets(buffer, BUFFER_SIZE, stdin);
-            *strchr(buffer, '\n') = 0;
-            write(to_server, buffer, BUFFER_SIZE);
-            printf("[client] wrote input: [%s]\n", buffer);
-	    */
+            /*
+               fgets(buffer, BUFFER_SIZE, stdin);
+             *strchr(buffer, '\n') = 0;
+             write(to_server, buffer, BUFFER_SIZE);
+             printf("[client] wrote input: [%s]\n", buffer);
+             */
 
         } else {
             /* *strchr(buffer, '\n') = 0; */
             printf("received: [%s]\n", buffer);
             strcpy(buffer, ACK);
             write(to_server, buffer, BUFFER_SIZE);
-            printf("[client] reading..\n");
         }
-	input[0] = 0;
+        input[0] = 0;
         buffer = zero_heap(buffer, BUFFER_SIZE);
     }
 }
