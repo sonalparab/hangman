9eac92115a982a26fe5d4ef3be461aaa20a8ecfc
sonalparab <sparab@stuy.edu>
2018-01-25 20:55:10 -0500

    Reject space/empty char

--- src/game.c
+++ src/game.c
@@ -176,34 +176,61 @@ void run_game(char * word, int client_socket) {
             //update k because a guess was made
             k = 0;
 
-            //if the character inputted was uppercase
-            if (strchr("ABCDEFGHIJKLMNOPQRSTUVWXYZ",letter) != NULL) {
-                strcpy(message,"Please input a lowercase letter next time");
+	    if (letter == ' '){
+	        strcpy(message,"A space is not a letter");
                 write(client_socket, message, BUFFER_SIZE);
                 printf("[subserver %d] Sent %s\n", pid, message);
                 test = read(client_socket, buffer, BUFFER_SIZE);
                 if (test == -1 || strcmp(buffer, ACK)) {
-                    printf("Error 4!");
+                    printf("Error 3.5!");
                 }
                 buffer = zero_heap(buffer, BUFFER_SIZE);
                 message = zero_heap(message, BUFFER_SIZE);
-                letter = tolower(letter);
-            }
-
-
-            //if the guess was not a letter
-            if (strchr("abcdefghijklmnopqrstuvwxyz",letter) == NULL) {
-                strcpy(message,"Not a valid letter");
+		k = 1;
+	    }
+	    else if (letter == NULL){
+	        strcpy(message,"An empty character is not a letter");
                 write(client_socket, message, BUFFER_SIZE);
                 printf("[subserver %d] Sent %s\n", pid, message);
                 test = read(client_socket, buffer, BUFFER_SIZE);
                 if (test == -1 || strcmp(buffer, ACK)) {
-                    printf("Error 5!");
+                    printf("Error 3.7!");
                 }
                 buffer = zero_heap(buffer, BUFFER_SIZE);
                 message = zero_heap(message, BUFFER_SIZE);
-                k = 1;
-            } else{
+		k = 1;
+	    }
+
+	    if(!k){
+	        //if the character inputted was uppercase
+	        if (strchr("ABCDEFGHIJKLMNOPQRSTUVWXYZ",letter) != NULL) {
+		    strcpy(message,"Please input a lowercase letter next time");
+		    write(client_socket, message, BUFFER_SIZE);
+		    printf("[subserver %d] Sent %s\n", pid, message);
+		    test = read(client_socket, buffer, BUFFER_SIZE);
+		    if (test == -1 || strcmp(buffer, ACK)) {
+		        printf("Error 4!");
+		    }
+		    buffer = zero_heap(buffer, BUFFER_SIZE);
+		    message = zero_heap(message, BUFFER_SIZE);
+		    letter = tolower(letter);
+		}
+
+
+                //if the guess was not a letter
+		if (strchr("abcdefghijklmnopqrstuvwxyz",letter) == NULL) {
+		    strcpy(message,"Not a valid letter");
+		    write(client_socket, message, BUFFER_SIZE);
+		    printf("[subserver %d] Sent %s\n", pid, message);
+		    test = read(client_socket, buffer, BUFFER_SIZE);
+		    if (test == -1 || strcmp(buffer, ACK)) {
+		        printf("Error 5!");
+		    }
+		    buffer = zero_heap(buffer, BUFFER_SIZE);
+		    message = zero_heap(message, BUFFER_SIZE);
+		    k = 1;
+		}
+	    } else{
                 i = 0;
                 for (;i < g;i++) {
                     //if the letter was already guessed
