97c8285ffeb86f2c1c78b01c0e4f3007d72262dd
sonalparab <sparab@stuy.edu>
2018-01-25 20:35:42 -0500

    Print word when you lose

--- src/game.c
+++ src/game.c
@@ -265,6 +265,18 @@ void run_game(char * word, int client_socket) {
             if (test == -1 || strcmp(buffer, ACK)) {
                 printf("Error 6!");
             }
+	    buffer = zero_heap(buffer, BUFFER_SIZE);
+	    message = zero_heap(message, BUFFER_SIZE);
+	    strcpy(message,"The word was: ");
+	    strcat(message,word);
+	    write(client_socket, message, BUFFER_SIZE);
+            printf("[subserver %d] Sent %s\n", pid, message);
+            test = read(client_socket, buffer, BUFFER_SIZE);
+            if (test == -1 || strcmp(buffer, ACK)) {
+                printf("Error 11!");
+            }
+            buffer = zero_heap(buffer, BUFFER_SIZE);
+            message = zero_heap(message, BUFFER_SIZE);
             free(buffer);
             free(message);
             free(guessing_array);
